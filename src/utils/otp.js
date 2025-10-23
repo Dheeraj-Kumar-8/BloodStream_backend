abfa0beda1c884077df8f83f3dff6ee9bad1aa73
Deepak Reddy <deepakry544@gmail.com>
2025-10-23 11:44:43 +0530

    chore(otp): support static demo code via OTP_STATIC_CODE

--- src/utils/otp.js
+++ src/utils/otp.js
@@ -2,6 +2,11 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 
 const generateNumericOtp = (digits = 6) => {
+  if (process.env.OTP_STATIC_CODE) {
+    // Always return configured static code (trim/pad to requested digits)
+    return process.env.OTP_STATIC_CODE.toString().padStart(digits, '0').slice(-digits);
+  }
+
   const max = 10 ** digits;
   const number = crypto.randomInt(0, max);
   return number.toString().padStart(digits, '0');
